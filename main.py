02fff9cbb0ef8cabd31ba179abb93538a4ec3fd8
Sanjay Kumar Sahoo <Sanjay.Sahoo@ENCORA.COM>
2021-08-14 00:07:25 +0530

    Heroku deployment

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request
+import joblib
+import numpy as np
+app = Flask(__name__)
+
+#Load the model 
+model = joblib.load('hiring_model.pkl') 
+
+@app.route('/')
+def hello():
+    return render_template('base.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    exp=request.form.get('Experience')
+    Test_Score=exp=request.form.get('Test_Score')
+    Interview_Score=exp=request.form.get('Interview_Score')
+    print('Your Experience' , exp)
+    print('Your Test Score is ' , Test_Score)
+    print('Your Interview Score is' , Interview_Score)
+
+    prediction = model.predict([[int(exp),int(Test_Score),int(Interview_Score)]])  # Model prediction based on user input data in browser 
+    print('Model prediction is : ' , prediction)
+    output = round(prediction[0],2)
+
+    #return 'Your form submitted successfull'
+    return render_template('base.html',prediction_text = f'Employee salary will be $ {output}')
+    
+app.run(debug=True)
